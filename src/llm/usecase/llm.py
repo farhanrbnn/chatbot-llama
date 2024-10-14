6e4cc0a3d8a922f353d0292289a7959f84a91bec
Farhan Rabbaanii <farhan.rabbaanii@prosia.co.id>
2024-10-15 01:12:10 +0700

    add conversational history

--- src/llm/usecase/llm.py
+++ src/llm/usecase/llm.py
@@ -1,49 +1,61 @@
 from langchain_ollama import ChatOllama
 from langchain import PromptTemplate
 from langchain.memory import ConversationBufferMemory
-
-
 from langchain.chains import ConversationChain
-
-from langchain_core.messages import SystemMessage
-from langchain_core.prompts import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    MessagesPlaceholder,
-)
+from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
+from langchain.schema import messages_from_dict, messages_to_dict
 
 class LLMusecase:
     def __init__(self) -> None:
         self.llm = ChatOllama(
-            model="llama3.1:8b",
-            temperature=0
+            model="llama3.1:8b", temperature=0
         )
+        self.store_chat = None 
+        self.prompt = """You are a chatbot having a conversation with a human. your name is Llama-Han it stands for Llama model and farhan as a creator. If the AI does not know the answer to a question, it truthfully says it does not know.
+                    Current conversation:
+                    {history}
+                    Human: {input}
+                    AI Assistant:"""
 
-        self.prompt = ChatPromptTemplate.from_messages(
-            [
-                SystemMessage(
-                    content="You are a chatbot having a conversation with a human. your name is Llama-Han"
-                ),  # The persistent system prompt
-                MessagesPlaceholder(
-                    variable_name="chat_history"
-                ),  # Where the memory will be stored.
-                HumanMessagePromptTemplate.from_template(
-                    "{input}"
-                ),  # Where the human input will injected
-            ]
-        )
+    def generate(self, request) -> dict:
+        predict = None
+        PROMPT = PromptTemplate(input_variables=["history", "input"], template=self.prompt)
 
-    def generate(self, request) -> None:
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        if self.store_chat is None:
+            memory = ConversationBufferMemory(memory_key="history", return_messages=True)
 
-        llm_chain = ConversationChain(
-            llm=self.llm,
-            prompt=self.prompt,
-            verbose=True,
-            memory=memory,
-        )
+            llm_chain = ConversationChain(
+                llm=self.llm,
+                prompt=PROMPT,
+                verbose=True,
+                memory=memory,
+            )
 
-        predict = llm_chain.predict(input=request["input"])
+            predict = llm_chain.predict(input=request["input"])
+
+            extracted_messages = llm_chain.memory.chat_memory.messages
+            to_dict = messages_to_dict(extracted_messages)
+
+            self.store_chat = to_dict
+        else:
+            retrieved_message = messages_from_dict(self.store_chat)
+            retrieved_chat_history = ChatMessageHistory(messages=retrieved_message)
+
+            retrieved_memory = ConversationBufferMemory(chat_memory=retrieved_chat_history)
+
+            reload_chain = ConversationChain(
+                llm=self.llm,
+                prompt=PROMPT,
+                verbose=True,
+                memory=retrieved_memory,
+            ) 
+ 
+            predict = reload_chain.predict(input=request["input"])
+
+            extracted_messages = reload_chain.memory.chat_memory.messages
+            to_dict = messages_to_dict(extracted_messages)
+
+            self.store_chat = to_dict
 
         return {
             "response": predict
